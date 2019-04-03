b623a294e82fdd018dfc4a4939f9e9b744f411b3
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-03 17:37:40 -0400

    debug

--- signup-submit.php
+++ signup-submit.php
@@ -35,12 +35,6 @@
         $usr['max_seek_age'] = ($_POST['max_seek_age']);
     }
 
-//full name validation
-    if ( preg_match(" /[^a-zA-Z\s]/ ", $_POST["name"]) == 1) {
-        $ERR = "Name can only contain alphabets"
-    } 
-
-
 
 ?>
 
