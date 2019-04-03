d357f922111c4ab3c664b0f747d82dd069b5dd18
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-03 14:13:21 -0400

    added signup form

---
+++ signup.php
@@ -0,0 +1,39 @@
+<?php include("top.html");?>
+
+
+<div>
+    <!-- form to create new account -->
+    <form action="signup-submit.php" method="post">
+        <fieldset>
+            <legend>New User Signup:</legend>
+            <strong class="column">Name:</strong>
+            <input type="text" name="name" size="16" maxlength="16"><br>
+            
+            <!-- initially female must be checked -->
+            <strong class="column">Gender:</strong>
+            <input type="radio" name="gender" value="M">Male
+            <input type="radio" name="gender" value="F" checked>Female<br>
+
+            <strong class="column">Age:</strong>
+            <input type="text" name="age" size="6" maxlength="2"><br>
+
+            <input type="text" name="personality_type" size="6" maxlength="4"><br>
+
+            <strong class="column">Favorite OS:</strong>
+            <select name="os">
+                <option value="Windows" selected>Windows</option>
+                <option value="Mac OS X" >Mac OS X</option>
+                <option value="Linux"> Linux</option>
+            </select><br>
+
+            <strong class="column">Seeking age:</strong>
+            <input type="text" name="min_seeking_age" size="6" maxlength="2" placeholder="min"> to
+            <input type="text" name="max_seeking_age" size="6" maxlength="2" placeholder="max"><br>
+
+            <input type="submit" value="Sign Up">
+
+        </fieldset>
+        
+    </form>
+</div>
+
