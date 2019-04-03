57ed74f5b3a51a1e6c818c45de522b787db50e4f
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-03 19:46:37 -0400

    error message banner

--- signup-submit.php
+++ signup-submit.php
@@ -1,57 +1,57 @@
 <?php include("top.html"); ?>
 
 <?php
-$ERR = array();
-$usr = array(
-    'name' => '',
-    'gender' => '',
-    'age' => '',
-    'persona_type' => '',
-    'fav_os' => '',
-    'min_seek_age' => '',
-    'max_seek_age' => ''
-);
+    $ERR = array();
+    $usr = array(
+        'name' => '',
+        'gender' => '',
+        'age' => '',
+        'persona_type' => '',
+        'fav_os' => '',
+        'min_seek_age' => '',
+        'max_seek_age' => ''
+    );
 
-/*Extract data from post request (associative array)*/
-if(isset($_POST['name'])) {
-    $usr['name'] = urlencode($_POST['name']);
-}
-if(isset($_POST['gender'])) {
-    $usr['gender'] = urlencode($_POST['gender']);
-}
-if(isset($_POST['age'])) {
-    $usr['age'] = urlencode($_POST['age']);
-}
-if(isset($_POST['persona_type'])) {
-    $usr['persona_type'] = ($_POST['persona_type']);
-}
-if(isset($_POST['fav_os'])) {
-    $usr['fav_os'] = ($_POST['fav_os']);
-}
-if(isset($_POST['min_seek_age'])){
-    $usr['min_seek_age'] = ($_POST['min_seek_age']);
-}
-if(isset($_POST['max_seek_age'])){
-    $usr['max_seek_age'] = ($_POST['max_seek_age']);
-}
-$len0 = 0;
-    //full name validation
-if ( preg_match(" /[^a-zA-Z\s]/ ", $_POST["name"]) === 1) {
-    $ERR[] = "Name should be valid character string";
-    $len0 = 1;
-} 
-
-if (strlen($_POST['name'])==0){
-    $ERR[] = "Name cannot be empty";
-    $len0 = 1;
-}
-    $full_name = explode(" ", $_POST['name']); //delimited space
-    for ($i = 0; $i < count($full_name); $i++) {
-        if(strcmp(ucfirst($full_name[$i]),$full_name[$i]) !== 0) {
-            $ERR[] = "First character of Name must be CAPITAL!!";
-            break;
-        }
+    /*Extract data from post request (associative array)*/
+    if(isset($_POST['name'])) {
+        $usr['name'] = urlencode($_POST['name']);
     }
+    if(isset($_POST['gender'])) {
+        $usr['gender'] = urlencode($_POST['gender']);
+    }
+    if(isset($_POST['age'])) {
+        $usr['age'] = urlencode($_POST['age']);
+    }
+    if(isset($_POST['persona_type'])) {
+        $usr['persona_type'] = ($_POST['persona_type']);
+    }
+    if(isset($_POST['fav_os'])) {
+        $usr['fav_os'] = ($_POST['fav_os']);
+    }
+    if(isset($_POST['min_seek_age'])){
+        $usr['min_seek_age'] = ($_POST['min_seek_age']);
+    }
+    if(isset($_POST['max_seek_age'])){
+        $usr['max_seek_age'] = ($_POST['max_seek_age']);
+    }
+    $len0 = 0;
+        //full name validation
+    if ( preg_match(" /[^a-zA-Z\s]/ ", $_POST["name"]) === 1) {
+        $ERR[] = "Name should be valid character string";
+        $len0 = 1;
+    } 
+
+    if (strlen($_POST['name'])==0){
+        $ERR[] = "Name cannot be empty";
+        $len0 = 1;
+    }
+        $full_name = explode(" ", $_POST['name']); //delimited space
+        for ($i = 0; $i < count($full_name); $i++) {
+            if(strcmp(ucfirst($full_name[$i]),$full_name[$i]) !== 0) {
+                $ERR[] = "First character of Name must be CAPITAL!!";
+                break;
+            }
+        }
     //age validation
     if (!is_numeric($usr["age"]) || (int) $usr >= 200 ) {
         $ERR[] = "Age must be a sensible number!";
@@ -69,10 +69,26 @@ if (strlen($_POST['name'])==0){
         $ERR[] = "Maximum age sought must be a number.";
     }
 
-    foreach ($ERR as $e) {
-        print($e)."\n";
+    ?>
+
+    <!-- Input validation done -->
+    <?php
+        if(!empty($ERR)){
+        foreach ($ERR as $e) {
+    
+    ?>
+        <h1>Error...</h1>
+        <ul>
+            <li><?= $e?></li>
+        </ul>
+    <?php
+        }
+    }else{
+
     }
 
+
     ?>
+
 
     <?php include("bottom.html"); ?>
