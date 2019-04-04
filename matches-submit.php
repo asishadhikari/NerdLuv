c085dfa344aac33ad9be123656950d182e2eefd4
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-04 15:13:30 -0400

    changing queries

--- matches-submit.php
+++ matches-submit.php
@@ -6,13 +6,13 @@ require_once('init_connection.php');
 $given_name = $_GET['name'];
 global $dbase;
 
+$uid = ''; 
+$age = '';
 $gender = '';
 $persona_type = '';
-$fav_os = '';
 $min_seek_age = '';
 $max_seek_age = '';
-$age = '';
-$uid = ''; 
+$fav_os = '';
 
 	//query dbase
 $stmt = "SELECT * FROM users where name = '" . $given_name . "';";
@@ -24,29 +24,20 @@ $uid = $record["id"];
 $gender = $record["gender"];
 $age = (int)$record["age"];
 
-$stmt = "SELECT name FROM personalities WHERE user_id = ";
-$stmt .= $uid . ";";
+//obtain user's personality type
+$stmt = "SELECT name FROM personalities WHERE user_id = ".$uid;
 $res_personality = mysqli_query($dbase, $stmt);
 $persona_type = $res_personality->fetch_assoc()["name"];
 
-$stmt = "SELECT name FROM fav_os WHERE user_id = ";
-$stmt .= $user_id . ";";
+//obtain user's favourite os
+$stmt = "SELECT name FROM fav_os WHERE user_id = ".$uid;
 $res_favos = mysqli_query($dbase, $stmt);
 $fav_os = $res_favos->fetch_assoc()["name"];
 
-$stmt = "SELECT min_age, max_age FROM seeking_age ";
-$stmt .= "WHERE user_id = ";
-$stmt .= $uid . ";";
+//obtain the seeking age
+$stmt = "SELECT min_age, max_age FROM seeking_age WHERE user_id = ".$uid;
 $res_seek_age = mysqli_query($dbase, $stmt);
 $seek_age = $res_seek_age->fetch_assoc();
-$min_seek_age = (int)$seek_age["min_age"];
-$max_seek_age = (int)$seek_age["max_age"];
-
-$stmt = "SELECT min_age, max_age FROM seeking_age ";
-$stmt .= "WHERE user_id = ";
-$stmt .= $uid . ";";
-$res_seek_age = mysqli_query($dbase, $stmt);
-$seek_ages = $res_seek_age->fetch_assoc();
 $min_seek_age = (int)$seek_age["min_age"];
 $max_seek_age = (int)$seek_age["max_age"];
 
@@ -54,23 +45,7 @@ $max_seek_age = (int)$seek_age["max_age"];
 //Since gender is submitted with radio, not input validating here
 $seeking_gender =  (strcmp($gender,'M')==0 ? 'F' : 'M');
 $candidate = array();
-/*
-$stmt = "SELECT users.name as name, gender, age, ";
-$stmt .= "fav_os.name as os, ";
-$stmt .= "personalities.name as personality FROM users ";
-$stmt .= "JOIN fav_os ON users.id = fav_os.user_id ";
-$stmt .= "JOIN seeking_age ON users.id = seeking_age.user_id ";
-$stmt .= "JOIN personalities ON users.id = personalities.user_id ";
-$stmt .= "WHERE users.gender = ";
-$stmt .= "'" . $seeking_gender . "' ";
-$stmt .= "and users.age >= ". $min_seek_age . " ";
-$stmt .= "and users.age <= ". $max_seek_age . " ";
-$stmt .= "and seeking_age.min_age <= " . $age . " ";
-$stmt .= "and seeking_age.max_age >= " . $age . " ";
-$stmt .= "and fav_os.name = '" . $fav_os . "'; ";
 
-
-*/
 $stmt = "SELECT users.name AS name, gender, age, personalities.name AS personality, fav_os.name as os FROM users, personalities, fav_os, seeking_age WHERE users.id = personalities.user_id = fav_os.user_id = seeking_age.user_id AND gender = ".$seeking_gender." AND age >= ".$min_seek_age." AND age <= ".$max_seek_age." AND ";
 
 $query = mysqli_query($dbase, $stmt);
