c0ed7a89a57e2210e94f746da175d200006d92bd
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-04 17:23:41 -0400

    debug

--- matches-submit.php
+++ matches-submit.php
@@ -74,6 +74,45 @@ $stmt .= "and seeking_age.max_age >= " . $age . " ";
 $stmt .= "and fav_os.name = '" . $fav_os . "'; ";
 
 $query = mysqli_query($dbase, $stmt);
-print($query->num_rows);
+
+if ($query->num_rows > 0) {
+?>
+	<strong>Matches for <?= $_GET['name'] ?></strong><br>
+	<div>
+<?php
+		while ($record = mysqli_fetch_row($query)) {
+			//check if a compatible personality
+			$persona_check = "/[".$persona_type."]/";
+			if (preg_match($persona_check, $record["personality"]) === 1){
+?>
+				<div class="match">
+					<img src="user.jpg" alt="photo"/>
+					<div>
+						<ul>
+							<li><p><?= $record["name"] ?></p></li>
+							<li><strong>gender:</strong> <?= $record["gender"] ?></li>
+							<li><strong> age:</strong> <?= $record["age"] ?> </li>
+							<li><strong> type:</strong> <?= $record["personality"] ?> </li>
+							<li><strong> OS:</strong> <?= $record["os"] ?></li>
+						</ul>
+					</div>
+				</div>
+<?php
+			}
+		}
+print("nok...");
+?>
+	</div>
+
+<?php
+}else{
+print("ok...");
+
+?>
+	<div>
+	<p> No match is found.</p>
+	</div>
+<?php
+}
 include('bottom.html');
 ?>
