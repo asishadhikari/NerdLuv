b60591f4614b4d7926806b7d6a897dc7653a3673
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-04 15:57:22 -0400

    restructure sql query

--- matches-submit.php
+++ matches-submit.php
@@ -51,6 +51,8 @@ $candidate = array();
 
 $stmt = "SELECT users.name AS name, gender, age, personalities.name AS personality, fav_os.name as os FROM users, personalities, fav_os, seeking_age WHERE users.id = personalities.user_id = fav_os.user_id = seeking_age.user_id AND gender = '".$seeking_gender."' AND age >= ".$min_seek_age." AND age <= ".$max_seek_age." AND fav_os = '".$fav_os."';";
 
+print($stmt);
+
 $query = mysqli_query($dbase, $stmt);
 if ($query->num_rows > 0) {
 	?>
