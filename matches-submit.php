0f9e31063947ba4f4c6f7745d7ce32856b8a98f7
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-04 15:05:46 -0400

    dont worry about duplicate users, using first result from query

--- matches-submit.php
+++ matches-submit.php
@@ -18,47 +18,44 @@ $uid = '';
 $stmt = "SELECT * FROM users where name = '" . $given_name . "';";
 $matches = mysqli_query($dbase, $stmt);
 
+//not worrying about duplicates and using first result
+$record = $matches->fetch_assoc()) 
+$uid = $record["id"];
+$gender = $record["gender"];
+$age = (int)$record["age"];
 
-if ($matches->num_rows > 0) {
-	while ($record = $matches->fetch_assoc()) {
-		$uid = $record["id"];
-		$gender = $record["gender"];
-		$age = (int)$record["age"];
+$stmt = "SELECT name FROM personalities WHERE user_id = ";
+$stmt .= $uid . ";";
+$res_personality = mysqli_query($dbase, $stmt);
+$persona_type = $res_personality->fetch_assoc()["name"];
 
-		$stmt = "SELECT name FROM personalities WHERE user_id = ";
-		$stmt .= $uid . ";";
-		$res_personality = mysqli_query($dbase, $stmt);
-		$persona_type = $res_personality->fetch_assoc()["name"];
+$stmt = "SELECT name FROM fav_os WHERE user_id = ";
+$stmt .= $user_id . ";";
+$res_favos = mysqli_query($dbase, $stmt);
+$fav_os = $res_favos->fetch_assoc()["name"];
 
-		$stmt = "SELECT name FROM fav_os WHERE user_id = ";
-		$stmt .= $user_id . ";";
-		$res_favos = mysqli_query($dbase, $stmt);
-		$fav_os = $res_favos->fetch_assoc()["name"];
+$stmt = "SELECT min_age, max_age FROM seeking_age ";
+$stmt .= "WHERE user_id = ";
+$stmt .= $uid . ";";
+$res_seek_age = mysqli_query($dbase, $stmt);
+$seek_age = $res_seek_age->fetch_assoc();
+$min_seek_age = (int)$seek_age["min_age"];
+$max_seek_age = (int)$seek_age["max_age"];
 
-		$stmt = "SELECT min_age, max_age FROM seeking_age ";
-		$stmt .= "WHERE user_id = ";
-		$stmt .= $uid . ";";
-		$res_seek_age = mysqli_query($dbase, $stmt);
-		$seek_age = $res_seek_age->fetch_assoc();
-		$min_seek_age = (int)$seek_age["min_age"];
-		$max_seek_age = (int)$seek_age["max_age"];
+$stmt = "SELECT min_age, max_age FROM seeking_age ";
+$stmt .= "WHERE user_id = ";
+$stmt .= $uid . ";";
+$res_seek_age = mysqli_query($dbase, $stmt);
+$seek_ages = $res_seek_age->fetch_assoc();
+$min_seek_age = (int)$seek_age["min_age"];
+$max_seek_age = (int)$seek_age["max_age"];
 
-		$stmt = "SELECT min_age, max_age FROM seeking_age ";
-		$stmt .= "WHERE user_id = ";
-		$stmt .= $uid . ";";
-		$res_seek_age = mysqli_query($dbase, $stmt);
-		$seek_ages = $res_seek_age->fetch_assoc();
-		$min_seek_age = (int)$seek_age["min_age"];
-		$max_seek_age = (int)$seek_age["max_age"];
-
-	}
-}
 
 //Since gender is submitted with radio, not input validating here
 $seeking_gender =  (strcmp($gender,'M')==0 ? 'F' : 'M');
 $candidate = array();
-
-$stmt = "SELECT users.name, gender, age, ";
+/*
+$stmt = "SELECT users.name as name, gender, age, ";
 $stmt .= "fav_os.name as os, ";
 $stmt .= "personalities.name as personality FROM users ";
 $stmt .= "JOIN fav_os ON users.id = fav_os.user_id ";
@@ -72,8 +69,46 @@ $stmt .= "and seeking_age.min_age <= " . $age . " ";
 $stmt .= "and seeking_age.max_age >= " . $age . " ";
 $stmt .= "and fav_os.name = '" . $fav_os . "'; ";
 
+
+*/
+$stmt = "SELECT users.name AS name, gender, age, personalities.name AS personality, fav_os.name as os FROM users, personalities, fav_os, seeking_age WHERE users.id = personalities.user_id = fav_os.user_id = seeking_age.user_id AND gender = ".$seeking_gender." AND age >= ".$min_seek_age." AND age <= ".$max_seek_age." AND ";
+
 $query = mysqli_query($dbase, $stmt);
 
+if ($query->num_rows > 0) {
+	?>
+	<strong>Matches for <?= $_GET['name'] ?></strong><br>
+	<div>
+		<?php
+		while ($record = $query->fetch_assoc()) {
+			//check if a compatible personality
+			$persona_check = "/[".$persona_type."]/";
+			if (preg_match($persona_check, $record["personality"]) === 1){
+				?>
+				<!--Display Matches  -->
+				<div class="match">
+					<img src="user.jpg" alt="photo"/>
+					<div>
+						<ul>
+							<li><p><?= $record["name"] ?></p></li>
+							<li><strong>gender:</strong> <?= $record["gender"] ?></li>
+							<li><strong> age:</strong> <?= $record["age"] ?> </li>
+							<li><strong> type:</strong> <?= $record["personality"] ?> </li>
+							<li><strong> OS:</strong> <?= $record["os"] ?></li>
+						</ul>
+					</div>
+				</div>
+				<?php
+			}
+		}
+		?>
+	</div>
+	<?php
+}else{
+	?>
+	<p> No match is found.</p>
+	<?php	
+}
 
 
 include('bottom.html');
