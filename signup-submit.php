3101e6fedee64b5555681075d5f1566774c8a78c
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-03 20:08:16 -0400

    debug

--- signup-submit.php
+++ signup-submit.php
@@ -71,7 +71,7 @@
 
 
     if ( isset($usr['min_seek_age']) && isset($usr['max_seek_age'])) {
-        if( (int) $min_seek_age > (int) $max_seek_age){
+        if( (int) $usr['min_seek_age'] > (int) $usr['min_seek_age']){
             $ERR[] = "Your seeking age is inconsistent!";
         }
     }
