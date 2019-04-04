2d1165ee59c9d657e30f62832b04e3ae21256223
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-04 17:24:52 -0400

    debug

--- matches-submit.php
+++ matches-submit.php
@@ -74,7 +74,6 @@ $stmt .= "and seeking_age.max_age >= " . $age . " ";
 $stmt .= "and fav_os.name = '" . $fav_os . "'; ";
 
 $query = mysqli_query($dbase, $stmt);
-
 if ($query->num_rows > 0) {
 ?>
 	<strong>Matches for <?= $_GET['name'] ?></strong><br>
