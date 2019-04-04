0660033072d3a63cede1480c7256c718328d1d70
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-04 00:47:33 -0400

    experimenting with a database layout

--- NERDLUV.sql
+++ NERDLUV.sql
@@ -9,21 +9,41 @@ DROP TABLE IF EXISTS fav_os;
 DROP TABLE IF EXISTS seeking_age;
 
 CREATE TABLE users (
-    id int(11) NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     name varchar(255) DEFAULT NULL,
     gender varchar(1) DEFAULT NULL,
-    age int(11) UNSIGNED DEFAULT 0,
+    age int UNSIGNED DEFAULT 0,
     created_at datetime DEFAULT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE personalities (
-    id int(11) NOT NULL AUTO_INCREMENT,
+    id int NOT NULL AUTO_INCREMENT,
     name varchar(4) NOT NULL,
-    user_id int(11) NOT NULL,
+    user_id int NOT NULL,
     PRIMARY KEY (id),
     KEY user_id (user_id)
 );
+
+CREATE TABLE fav_os (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    user_id int NOT NULL,
+    PRIMARY KEY (id),
+    KEY user_id (user_id)
+);
+
+
+CREATE TABLE seeking_age (
+    id int NOT NULL AUTO_INCREMENT,
+    min_age int DEFAULT 0,
+    max_age int DEFAULT 50,
+    user_id int NOT NULL,
+    PRIMARY KEY (id),
+    KEY user_id (user_id)
+);
+
+
 
 
 
