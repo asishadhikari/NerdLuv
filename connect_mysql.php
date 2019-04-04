0619f306a760cf18cd2a9e6110e3df9babff3d36
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-03 23:13:16 -0400

    debug

--- connect_mysql.php
+++ connect_mysql.php
@@ -1,7 +1,7 @@
 <?php                                                                           
   define("DB_SRV", "localhost");                                             
   define("DB_USR", "root");                                                    
-  define("DB_PASS", "USER1PASSWORD");                                                
+  define("DB_PASS", "123QWEasd");                                                
   define("DB_NAME", "NERDLUV");                                                    
 
  ?>  
