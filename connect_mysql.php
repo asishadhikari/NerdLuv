fbe9f80c1e6e57159529dfd1c06cb8aced5360cc
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-04 15:58:22 -0400

    restructure sql query

--- connect_mysql.php
+++ connect_mysql.php
@@ -5,3 +5,6 @@
   define("DB_NAME", "NERDLUV");                                                    
 
 ?>  
+
+
+<!-- SELECT users.name AS name, gender, age, personalities.name AS personality, fav_os.name as os FROM users, personalities, fav_os, seeking_age WHERE users.id = personalities.user_id = fav_os.user_id = seeking_age.user_id AND gender = 'F' AND age >= 20 AND age <= 25 AND fav_os = 'Windows';  -->
