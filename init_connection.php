3cb96377525b048f9aee51b03a37b993bd696f97
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-03 23:11:54 -0400

    debug

--- init_connection.php
+++ init_connection.php
@@ -3,7 +3,7 @@ require_once('connect_mysql.php');
 header('X-XSS-Protection: 0');                    
 
 function connect_to_db() {                                                          
-	$conn = mysqli_connect(DB_SERV, DB_USR, DB_PASS, DB_NAME);            
+//	$conn = mysqli_connect(DB_SERV, DB_USR, DB_PASS, DB_NAME);            
 	if(mysqli_connect_errno()) {                                                   
 		$err = "Failed to initiate database connection. ";                                       
 		$err .= mysqli_connect_error();                                              
