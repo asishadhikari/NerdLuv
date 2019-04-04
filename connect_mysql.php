4c036585a1cae233ac0afe5bfff4139cebe11318
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-04-03 22:45:57 -0400

    working on connecting with database

---
+++ connect_mysql.php
@@ -0,0 +1,7 @@
+<?php                                                                           
+  define("DB_SRV", "localhost");                                             
+  define("DB_USR", "user1");                                                    
+  define("DB_PASS", "USER1PASSWORD");                                                
+  define("DB_NAME", "NERDLUV");                                                    
+
+ ?>  
---
+++ init_connection.php
@@ -0,0 +1,16 @@
+<?php
+require_once('connect_mysql.php');
+function connect_to_db() {                                                          
+	$conn = mysqli_connect(DB_SERV, DB_USR, DB_PASS, DB_NAME);            
+	if(mysqli_connect_errno()) {                                                   
+		$err = "Failed to initiate connection. ";                                       
+		$err .= mysqli_connect_error();                                              
+		$err .= " (" . mysqli_connect_errno() . ")";                                 
+		exit($msg);                                                                  
+	}                                                                              
+	return $conn;                                                            
+}   
+
+$dbase = connect_to_db();                                  
+
+?>
